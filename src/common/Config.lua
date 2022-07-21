22846e13af5092c3572fe0adc08d3e92ed11a69a
Daniel Murfet <daniel.murfet@gmail.com>
2022-07-22 09:34:55 +1000

    Added Moonlight Forest.

--- src/common/Config.lua
+++ src/common/Config.lua
@@ -13,14 +13,26 @@ local Config = {
 		["Gamma Events"] = 9240578087,
 		["Songspires"] = 9440651873,
 		["Gemini Pulsar"] = 9795154224,
-		["Symbolic Wilds"] = 10088371393},
+		["Symbolic Wilds"] = 10088371393,
+		["Moonlight Forest"] = 10302055084},
 	AvailablePockets = {
 		"Alpha Cove",
 		"Delta Plains",
 		"Gamma Events",
 		"Songspires",
 		"Symbolic Wilds"
+	},
+	PocketBackgrounds = {
+		["Alpha Cove"] = "rbxassetid://9409223438",
+		["Symbolic Wilds"] = "rbxassetid://10121182717",
+		["Gamma Events"] = "rbxassetid://9409218005",
+		["Delta Plains"] = "rbxassetid://9409191683",
+		["Gemini Pulsar"] = "rbxassetid://9795255250",
+		["Songspires"] = "rbxassetid://9444031149",
+		["Moonlight Forest"] = "rbxassetid://9444031149",
+		["_PocketLink"] = "rbxassetid://10197036910"
 	}
+	
 }
 
 return Config
